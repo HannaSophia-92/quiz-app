a04c0baf2f6cda3964ce50f0c42dfafeaebc6f86
Hanna Sophia Schlenker <hanna-schlenker@web.de>
2022-02-18 20:33:19 +0100

    Fix problems with js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import navigation from './js/navigation.js';
 import bookmarks from './js/bookmarks.js';
-import cards from './js/cards.js';
+
 import showAndHideButton from './js/show-hide.js';
 
 navigation();
 bookmarks();
-cards();
 showAndHideButton();
