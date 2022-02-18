bd21d35f2fddf4dd99a526f371582c07a4c09a95
Hanna Sophia Schlenker <hanna-schlenker@web.de>
2022-02-18 19:23:30 +0100

    Add card.js

--- js/cards.js
+++
@@ -1,9 +0,0 @@
-function cards() {
-  const cardSections = document.querySelectorAll('[data-js="cards"]');
-
-  cardSections.forEach(card => {
-    const showAnswerButton = card.querySelector('[data-js="show-answer"]');
-  });
-}
-
-export default cards;
