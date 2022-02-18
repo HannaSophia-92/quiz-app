e21ae4a7675f1fc16ce62e3714a9e3060d98f606
Hanna Sophia Schlenker <hanna-schlenker@web.de>
2022-02-18 18:01:14 +0100

    Add js to navigation.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-console.clear();
+import navigation from './js/navigation.js';
 
-// const toggleButton = document.querySelector(".card__bookmark");
-
-// toggleButton.addEventListener("click", () => {
-//   toggleButton.classList.toggle("card__active");
-// });
-
-const toggleButton = document.querySelector('[data-js=toggleBookmark]');
-
-toggleButton.addEventListener('click', () => {
-  toggleButton.classList.toggle('card__active');
-});
-
-console.log(toggleButton);
+navigation();
---
+++ js/navigation.js
@@ -0,0 +1,46 @@
+console.clear();
+
+function navigation() {
+  const pageHome = document.querySelector('[data-js="page-home"]');
+  const pageBookmarks = document.querySelector('[data-js="page-bookmarks"]');
+  const pageCreate = document.querySelector('[data-js="page-create"]');
+  const pageProfile = document.querySelector('[data-js="page-profile"]');
+
+  const buttonHome = document.querySelector('[data-js="button-home"]');
+  const buttonBookmarks = document.querySelector(
+    '[data-js="button-bookmarks"]'
+  );
+  const buttonCreate = document.querySelector('[data-js="button-create"]');
+  const buttonProfile = document.querySelector('[data-js="button-profile"]');
+
+  buttonHome.addEventListener('click', () => {
+    pageHome.classList.remove('hidden');
+    pageBookmarks.classList.add('hidden');
+    pageCreate.classList.add('hidden');
+    pageProfile.classList.add('hidden');
+  });
+
+  buttonBookmarks.addEventListener('click', () => {
+    pageHome.classList.add('hidden');
+    pageBookmarks.classList.remove('hidden');
+    pageCreate.classList.add('hidden');
+    pageProfile.classList.add('hidden');
+  });
+
+  buttonCreate.addEventListener('click', () => {
+    pageHome.classList.add('hidden');
+    pageBookmarks.classList.add('hidden');
+    pageCreate.classList.remove('hidden');
+    pageProfile.classList.add('hidden');
+  });
+
+  buttonProfile.addEventListener('click', () => {
+    pageHome.classList.add('hidden');
+    pageBookmarks.classList.add('hidden');
+    pageCreate.classList.add('hidden');
+    pageProfile.classList.remove('hidden');
+  });
+  console.log(pageHome);
+}
+
+export default navigation;
