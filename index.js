70957e592cfd3fb0a93d3c49372c558d02ce9763
Hanna Sophia Schlenker <hanna-schlenker@web.de>
2022-02-24 22:34:42 +0100

    Add js to create dynamic cards

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import navigation from './js/navigation.js';
 import { cards, renderCards } from './js/cards.js';
 import counter from './js/counter.js';
+import create from './js/create.js';
 
 navigation();
+counter();
 cards();
 renderCards();
-counter();
+
+create();
