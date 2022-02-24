f721792480180204d63d6d53672e610bb57543af
Hanna Sophia Schlenker <hanna-schlenker@web.de>
2022-02-24 20:02:43 +0100

    Add dynamic cards and bookmark

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import navigation from './js/navigation.js';
-import cards from './js/cards.js';
+import { cards, renderCards } from './js/cards.js';
 import counter from './js/counter.js';
 
 navigation();
 cards();
+renderCards();
 counter();
--- js/cards.js
+++ js/cards.js
@@ -1,5 +1,72 @@
 console.clear();
 
+const quizCards = document.querySelectorAll('[data-js="cards"]');
+
+const data = [
+  {
+    question: 'Which two nations share the longest border in the world?',
+    answer:
+      'Canada and the United States share the longest land border in the world. Canada - America: 8.893km.',
+    tags: ['world', 'mix'],
+  },
+  {
+    question: 'What is the nickname of Japan?',
+    answer: 'The Land of the rising sun.',
+    tags: ['world', 'mix'],
+  },
+];
+
+function renderCards(list) {
+  quizCards.innerHTML = '';
+  list.forEach(card => {
+    const cardElement = document.createElement('article');
+    cardElement.className = 'card';
+    cardElement.innerHTML = `
+  <button
+            class="card__bookmark"${card.isBookmarked ? ' card__active' : ''}" }
+            data-js="toggleBookmark"
+            type="button"
+            aria-pressed="false"
+          >
+            <i class="fa-solid fa-bookmark fa-3x"></i>
+          </button>
+          <h2 class="card__title">Question</h2>
+          <div class="card__question">${card.question}
+          </div>
+          <div class="card__button">
+            <button
+              type="button"
+              class="card__button--style"
+              data-js="show-answer"
+              aria-pressed="false"
+            >
+              Show Answer
+            </button>
+          </div>
+          <p class="card__answer hidden" data-js="card-answer">${
+            card.answer
+          }</p>
+          <ul class="card__tags--answer" role="list">${card.tags
+            .map(tag => ` <li class="card__tags--style">${tag}</li>`)
+            .join('')}
+          </ul>
+  `;
+    const section = document.querySelector('[data-js="page-home"]');
+    section.append(cardElement);
+
+    const toggleBookmarks = cardElement.querySelector(
+      '[data-js="toggleBookmark"]'
+    );
+    toggleBookmarks.addEventListener('click', () => {
+      card.isBookmarked = !card.isBookmarked;
+      toggleBookmarks.classList.toggle('card__active');
+      console.log(toggleBookmarks);
+    });
+  });
+}
+
+renderCards(data);
+
 function cards() {
   const quizCards = document.querySelectorAll('[data-js="cards"]');
 
@@ -29,4 +96,4 @@ function cards() {
   });
 }
 
-export default cards;
+export { cards, renderCards };
